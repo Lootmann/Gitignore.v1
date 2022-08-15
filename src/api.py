d2bd7b4b073a9926a01c944a0db0ec2821392233
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-08-15 11:04:40 +0900

    api.py: using cache.py, if cache exists, use this.

--- src/api.py
+++ src/api.py
@@ -9,23 +9,33 @@ class API:
     API_URL = "https://www.toptal.com/developers/gitignore/api/{}"
 
     @classmethod
-    def get_list(cls) -> list:
+    def get_gitignore_list_from_api(cls) -> list:
+        """
+        get each language ignore list
+
+        :return: ignore list like ["c", "cpp", "d", "elixir", ...]
+        """
+        response = requests.get(cls.LIST_URL)
+
+        if response.status_code != 200:
+            raise requests.RequestException(f"Can't get response from '{cls.LIST_URL}'")
+
+        return response.text.split("\n")
+
+    @classmethod
+    def get_gitignore_list(cls) -> list:
         """
         get each language .gitignore lists
+        if .gitignore list are cached, return this cache file(ordinary plain text)
+        if not, create cached.
 
         :return: language_names list via gitignore.is api
         """
         if Cache.exists():
             return Cache.get_ignore_list()
 
-        response = requests.get(cls.LIST_URL)
-
-        if response.status_code != 200:
-            raise requests.RequestException(f"Can't get response from '{cls.LIST_URL}'")
-
-        ignore_list = response.text.split("\n")
+        ignore_list = cls.get_gitignore_list_from_api()
         Cache.create_gitignore_list(ignore_list)
-
         return ignore_list
 
     @classmethod
