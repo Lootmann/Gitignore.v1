655bfa3e28f19bb6af8cb87b8e47cc2d2fd3a8f0
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-08-15 11:05:44 +0900

    cache.py, main.py: add new command 'update' - recached

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 import argparse
 
 from api import API
+from cache import Cache
 from cli import CLI
 
 
@@ -10,12 +11,18 @@ def main():
 
     parser.add_argument("--list", action="store_true", help="show args that can allow language names")
     parser.add_argument("-l", action="store_true", help="same on above")
+    parser.add_argument("--update", action="store_true", help="update gitignore list cache")
     parser.add_argument("languages", nargs="*")
 
     args = parser.parse_args()
 
     if args.list or args.l:
-        CLI.print_list(API.get_list())
+        CLI.print_list(API.get_gitignore_list())
+        return
+
+    if args.update:
+        Cache.update_gitignore_list(API.get_gitignore_list_from_api())
+        CLI.print("success to update gitignore-list")
         return
 
     if len(args.languages) > 0:
